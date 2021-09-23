d01af138c4bff0e5000f990af2305076f0c517df
Haq Nawaz <haqnawaz99@yahoo.com>
2021-09-23 15:46:45 +0500

    Hide counter implemented

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import "./App.css";
 
 function App() {
   const [showCounter, setShowCounter] = useState(false);
+
+  const hideCounter = () => {
+    setShowCounter(false);
+  };
   return (
     <div className="App">
       <p>Counter</p>
       <button onClick={() => setShowCounter(true)}>Show Counter</button>
-      {showCounter && <Counter />}
+      {showCounter && <Counter hideCounter={hideCounter} />}
     </div>
   );
 }
-
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 
-const Counter = () => {
+const Counter = (props) => {
   const [count, setCount] = useState(0);
   return (
     <div>
       <h1>The Count is {count}</h1>
       <button onClick={() => setCount(count + 1)}>Increment Counter </button>
+      <p></p>
+      <button onClick={props.hideCounter}>Increment Counter </button>
     </div>
   );
 };
