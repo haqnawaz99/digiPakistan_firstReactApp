41fb439e3bbf67f52f2ca80d7a3943b426bcd1b7
Haq Nawaz <haqnawaz99@yahoo.com>
2021-09-23 12:49:36 +0500

    Counter Implemented

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,13 @@
+import { useState } from "react";
+
 const Counter = () => {
-  return <h1>This is counter</h1>;
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>The Count is {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment Counter </button>
+    </div>
+  );
 };
 
 export default Counter;
